cf8db8c7d7abeb71fe6d57960f393816a53a718a
Oliver Bracevac <oliver.bracevac@gmail.com>
2014-05-15 15:18:10 +0200

    New Mapper and Fold API for FlatSyntax

--- src/main/scala/djc/lang/FlatSyntax.scala
+++ src/main/scala/djc/lang/FlatSyntax.scala
@@ -4,128 +4,83 @@ import util.Bag
 
 object FlatSyntax {
 
-  abstract class Prog {
-    def fold[T](f: Folder.Type[T]): T
+  abstract class Prog
 
-    def map(f: Mapper.Type): Prog
+  case class Def(x: Symbol, s: Prog, p: Prog) extends Prog
 
-    def toSyntaxProg: Syntax.Prog = throw new UnsupportedOperationException
-    def toSyntaxServer: Syntax.Server = throw new UnsupportedOperationException
-    def toSyntaxService: Syntax.Service = throw new UnsupportedOperationException
+  case class Par(ps: Bag[Prog]) extends Prog
+  object Par {
+    def apply(ps : Prog*): Par = new Par(Bag(ps:_*))
   }
 
-  case class Def(x: Symbol, s: Prog, p: Prog) extends Prog {
-    def fold[T](f: Folder.Type[T]): T = f._1(x, s.fold(f), p.fold(f))
+  case class Send(rcv: Prog, args: List[Prog]) extends Prog
+  object Send {
+    def apply(rcv: Prog, args: Prog*): Send = new Send(rcv, List(args:_*))
+  }
 
-    def map(f: Mapper.Type): Prog = f._1 match {
-      case None => Def(x, s.map(f), p.map(f))
-      case Some(g) => g(x, s, p)
+  case class Var(x: Symbol) extends Prog
+
+  case class ServiceRef(srv: Prog, x: Symbol) extends Prog
+
+  case class ServerImpl(rules: Bag[Rule]) extends Prog
+  object ServerImpl {
+    def apply(rules: Rule*): ServerImpl = new ServerImpl(Bag(rules:_*))
+  }
+
+  case class Rule(ps: Bag[Pattern], p: Prog)
+
+  case class Pattern(name: Symbol, params: List[Symbol])
+  object Pattern {
+    def apply(name: Symbol, params: Symbol*): Pattern = new Pattern(name, List(params:_*))
+  }
+
+  trait Mapper {
+    def apply(prog: Prog): Prog = map(prog)
+
+    def map(prog: Prog): Prog = prog match {
+      case Def(x, p1, p2) =>
+        Def(x, map(p1), map(p2))
+      case Par(ps) =>
+        Par(ps map map)
+      case Send(p, args) =>
+        Send(map(p), (args map map))
+      case Var(x) =>
+        Var(x)
+      case ServiceRef(p1, x) =>
+        ServiceRef(map(p1), x)
+      case ServerImpl(rs) =>
+        ServerImpl(rs map mapRule)
     }
 
-    override def toSyntaxProg = Syntax.Def(x, s.toSyntaxServer, p.toSyntaxProg)
-  }
-
-  case class Par(ps: Bag[Prog]) extends Prog {
-    def fold[T](f: Folder.Type[T]): T = f._2(ps map (_.fold(f)))
-
-    def map(f: Mapper.Type): Prog = f._2 match {
-      case None => Par(ps map (_.map(f)))
-      case Some(g) => g(ps)
+    def mapRule(rule: Rule): Rule = {
+      val Rule(ps, prog) = rule
+      Rule(ps map mapPattern, map(prog))
     }
 
-    override def toSyntaxProg = Syntax.Par(ps map (_.toSyntaxProg))
+    def mapPattern(pattern: Pattern): Pattern = pattern
   }
 
-  case class Send(rcv: Prog, args: List[Prog]) extends Prog {
-    def fold[T](f: Folder.Type[T]): T = f._3(rcv.fold(f), args map (_.fold(f)))
-
-    def map(f: Mapper.Type): Prog = f._3 match {
-      case None => Send(rcv.map(f), args map (_.map(f)))
-      case Some(g) => g(rcv, args)
+  trait Fold {
+    def fold[T](init: T)(prog: Prog): T = prog match {
+      case Def(x, p1, p2) =>
+        fold(fold(init)(p1))(p2)
+      case Par(ps) =>
+        ps.foldLeft(init)(fold(_)(_))
+      case Send(p, args) =>
+        args.foldLeft(fold(init)(p))(fold(_)(_))
+      case Var(x) =>
+        init
+      case ServiceRef(p1, x) =>
+        fold(init)(p1)
+      case ServerImpl(rs) =>
+        rs.foldLeft(init)(foldRule(_)(_))
     }
 
-    override def toSyntaxProg = Syntax.Send(rcv.toSyntaxService, args map (_.toSyntaxService))
-  }
-
-  case class Var(x: Symbol) extends Prog {
-    def fold[T](f: Folder.Type[T]): T = f._4(x)
-
-    def map(f: Mapper.Type): Prog = f._4 match {
-      case None => Var(x)
-      case Some(g) => g(x)
+    def foldRule[T](init: T)(rule: Rule): T = {
+      val Rule(ps, prog) = rule
+      fold((ps.foldLeft(init)(foldPattern(_)(_))))(prog)
     }
 
-    override def toSyntaxServer = Syntax.ServerVar(x)
-    override def toSyntaxService = Syntax.ServiceVar(x)
-  }
-
-  case class ServiceRef(srv: Prog, x: Symbol) extends Prog {
-    def fold[T](f: Folder.Type[T]): T = f._5(srv.fold(f), x)
-
-    def map(f: Mapper.Type): Prog = f._5 match {
-      case None => ServiceRef(srv.map(f), x)
-      case Some(g) => g(srv, x)
-    }
-
-    override def toSyntaxService = Syntax.ServiceRef(srv.toSyntaxServer, x)
-  }
-
-  case class ServerImpl(rules: Bag[Rule]) extends Prog {
-    def fold[T](f: Folder.Type[T]): T = f._6(rules map (_.fold(f)))
-
-    def map(f: Mapper.Type): Prog = f._6 match {
-      case None => ServerImpl(rules map (_.map(f)))
-      case Some(g) => g(rules)
-    }
-
-    override def toSyntaxServer = Syntax.ServerImpl(rules map (_.toSyntaxRule))
-  }
-
-  case class Rule(ps: Bag[Pattern], p: Prog) {
-    def fold[T](f: Folder.Type[T]): T = f._7(ps map (_.fold(f)), p.fold(f))
-
-    def map(f: Mapper.Type): Rule = f._7 match {
-      case None => Rule(ps map (_.map(f)), p.map(f))
-      case Some(g) => g(ps, p)
-    }
-
-    def toSyntaxRule = Syntax.Rule(ps map (_.toSyntaxPattern), p.toSyntaxProg)
-  }
-
-  case class Pattern(name: Symbol, params: List[Symbol]) {
-    def fold[T](f: Folder.Type[T]): T = f._8(name, params)
-
-    def map(f: Mapper.Type): Pattern = f._8 match {
-      case None => Pattern(name, params)
-      case Some(g) => g(name, params)
-    }
-
-    def toSyntaxPattern = Syntax.Pattern(name, params)
-  }
-
-  object Folder {
-    type FDef[T] = (Symbol, T, T) => T
-    type FPar[T] = Bag[T] => T
-    type FSend[T] = (T, List[T]) => T
-    type FVar[T] = Symbol => T
-    type FServiceRef[T] = (T, Symbol) => T
-    type FServerImpl[T] = Bag[T] => T
-    type FRule[T] = (Bag[T], T) => T
-    type FPattern[T] = (Symbol, List[Symbol]) => T
-
-    type Type[T] = (FDef[T], FPar[T], FSend[T], FVar[T], FServiceRef[T], FServerImpl[T], FRule[T], FPattern[T])
-  }
-
-  object Mapper {
-    type FDef = (Symbol, Prog, Prog) => Prog
-    type FPar = Bag[Prog] => Prog
-    type FSend = (Prog, List[Prog]) => Prog
-    type FVar = Symbol => Prog
-    type FServiceRef = (Prog, Symbol) => Prog
-    type FServerImpl = Bag[Rule] => Prog
-    type FRule = (Bag[Pattern], Prog) => Rule
-    type FPattern = (Symbol, List[Symbol]) => Pattern
-
-    type Type = (Option[FDef], Option[FPar], Option[FSend], Option[FVar], Option[FServiceRef], Option[FServerImpl], Option[FRule], Option[FPattern])
+    def foldPattern[T](init: T)(pattern: Pattern): T = init
   }
 }
--- src/main/scala/djc/lang/sem/FlatSubstitution.scala
+++ src/main/scala/djc/lang/sem/FlatSubstitution.scala
@@ -4,54 +4,76 @@ import util.Bag
 import djc.lang.FlatSyntax._
 
 object FlatSubstitution {
-  def subst(x: Symbol, p: Prog): Mapper.Type = {
-    (Some((x2: Symbol, s2: Prog, p2: Prog) => substDef(x, p, x2, s2, p2)), // Def
-     None, // Par
-     None, // Send
-     Some((x2: Symbol) => if (x == x2) p else Var(x2)), // Var
-     None, // ServiceRef
-     None, // ServerImpl
-     Some((ps: Bag[Pattern], rp: Prog) => substRule(x, p, ps, rp)), // Rule
-     None) // Pattern
+  case class Subst(x: Symbol, repl: Prog) extends Mapper {
+    lazy val replVars = FreeVars(repl)
+
+    override def map(p: Prog): Prog = p match {
+      case Def(x2, s2, p2) =>
+        val isThis = x == 'this
+        val captureAvoiding = !replVars.contains(x2)
+        lazy val x2fresh = gensym(x2, replVars)
+        lazy val p2fresh = Subst(x2, Var(x2fresh))(p2)
+        val (x2res, p2res) = if (captureAvoiding) (x2,p2) else (x2fresh,p2fresh)
+
+        if (isThis)
+          Def(x2res, s2, map(p2res))
+        else if (x == x2)
+          Def(x2, map(s2), p2)
+        else
+          Def(x2res, map(s2), map(p2res))
+
+      case Var(`x`) => repl
+
+      case _ => super.map(p)
+    }
+
+    override def mapRule(rule: Rule): Rule = {
+      val Rule(ps, prog) = rule
+      val boundNames = ps.flatMap(_.params).toList
+      val conflictingNames = boundNames filter replVars
+      val captureAvoiding = conflictingNames.isEmpty
+
+      lazy val replacements = conflictingNames zip gensyms(conflictingNames, replVars)
+      lazy val progfresh = replacements.foldLeft(prog) {
+        case (p, (xn, yn)) => Subst(xn, Var(yn))(p)
+      }
+      lazy val rename: Symbol => Symbol = replacements.toMap orElse {
+        case s: Symbol => s
+      }
+      lazy val psfresh = ps map {
+        pat => Pattern(pat.name, pat.params.map(rename))
+      }
+      val (psres, progres) = if (captureAvoiding) (ps, prog) else (psfresh, progfresh)
+
+      if (boundNames contains x)
+        rule
+      else
+        Rule(psres, map(progres))
+    }
   }
-  def substRule(x: Symbol, repl: Prog, ps: Bag[Pattern], p: Prog): Rule = {
-    val patVars = (ps map (_.fold(freeVars))).flatten
-    if (patVars contains x)
-      Rule(ps, p)
-    else
-      Rule(ps, p.map(subst(x, repl)))
+
+  object FreeVars extends Fold {
+    def apply(prog: Prog): Set[Symbol] = fold(Set[Symbol]())(prog)
+
+    def fold(init: Set[Symbol])(prog: Prog): Set[Symbol] = prog match {
+      case Def(x, p1, p2) =>
+        fold(fold(init)(p1) - 'this)(p2) - x
+      case Var(x) =>
+        init + x
+      case _ => super.fold(init)(prog)
+    }
+
+    def foldRule(init: Set[Symbol])(rule: Rule): Set[Symbol] = {
+      super.foldRule(init)(rule) -- rule.ps.flatMap(_.params)
+    }
   }
 
-  def substDef(x: Symbol, repl: Prog, x2: Symbol, s2: Prog, p2: Prog) = {
-    val isThis = x == 'this
-    val replVars = repl.fold(freeVars)
-    val captureAvoiding = !replVars.contains(x2)
-    lazy val x2fresh = gensym(x2, replVars)
-    lazy val p2fresh = p2.map(subst(x2, Var(x2fresh)))
-    val (x2res, p2res) = if (captureAvoiding) (x2,p2) else (x2fresh,p2fresh)
-
-    if (isThis)
-      Def(x2res, s2, p2res.map(subst(x, repl)))
-    else if (x == x2)
-      Def(x2, s2.map(subst(x, repl)), p2)
-    else
-      Def(x2res, s2.map(subst(x, repl)), p2res.map(subst(x, repl)))
+  def gensyms(l: List[Symbol], used: Set[Symbol]): List[Symbol] = l match {
+    case Nil => Nil
+    case s :: ss =>
+      val sfresh = gensym(s, used)
+      sfresh :: gensyms(ss, used + sfresh)
   }
-
-
-  val freeVars: Folder.Type[Set[Symbol]] = {
-    type R = Set[Symbol]
-    ((x: Symbol, ds: R, ps: R) => (ds-'this) ++ (ps-x), // Def
-     (xs: Bag[R]) => xs.flatten, // Par
-     (srv: R, args: List[R]) => srv ++ args.flatten, // Send
-     (x: Symbol) => Set(x), // Var
-     (srv: R, x: Symbol) => srv, // ServiceRef
-     (xs: Bag[R]) => xs.flatten, // ServerImpl
-     (ps: Bag[R], p: R) => p -- ps.flatten, // Rule
-     (name: Symbol, params: List[Symbol]) => params.toSet) // Pattern
-  }
-
-
   def gensym(x: Symbol, used: Set[Symbol]): Symbol = gensym(x, 0, used)
   def gensym(x: Symbol, i: Int, used: Set[Symbol]): Symbol = {
     val s = Symbol(s"${x.name}_$i")
