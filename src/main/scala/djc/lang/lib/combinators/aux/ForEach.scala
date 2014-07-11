edd1158177f8d9c5c1113ae762314b54a8d50cb3
Oliver Bracevac <oliver.bracevac@gmail.com>
2014-07-11 23:09:56 +0200

    Fix

--- src/main/scala/djc/lang/lib/combinators/aux/ForEach.scala
+++ src/main/scala/djc/lang/lib/combinators/aux/ForEach.scala
@@ -10,17 +10,21 @@ import djc.lang.lib.Function
 object ForEach {
   def apply(t: Type) = TApp(service, t)
 
-  val TForEachInt = TUniv('A, TSrv(TSrvRep('foreach -> ?(TList('A), TFun(TVar('A) -> Unit)))))
+  val TForEachInt = TUniv('A, TSrv(TSrvRep('foreach -> ?(TList('A)))))
   val TForEach = TUniv('A, ?(TList('A), TFun(TVar('A) -> Unit)))
 
   val service = TAbs('A) {
     LocalServer {
       Rule('foreach?('xs -> TList('A), 'body -> TFun(TVar('A) -> Unit))) {
-        Ifc(TForEachInt('A))('xs.isNil('A)) {
-          Par()
-        } Else {
-          'body!!('xs.head('A), Function.consume(Unit)) && 'this~>'foreach!!('xs.tail('A))
-        }
+        LocalServer {
+          Rule('foreach?('xs -> TList('A))) {
+            Ifc(TForEachInt('A))('xs.isNil('A)) {
+              Par()
+            } Else {
+              'body !!('xs.head('A), Function.consume(Unit)) && 'this ~> 'foreach !! ('xs.tail('A))
+            }
+          }
+        }~>'foreach!!('xs)
       }
     }~>'foreach
   }
